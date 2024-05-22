1255b188c1b6e6dcf5a795b610ac4e88c0584fdf
Mattia Forti <mattia.forti2@studio.unibo.it>
2024-05-22 10:40:48 +0200

    fix: replaced radio buttons with drop down menu for app theme

--- app/src/main/java/com/example/cgo/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/cgo/ui/screens/settings/SettingsScreen.kt
@@ -1,18 +1,22 @@
 package com.example.cgo.ui.screens.settings
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.RadioButton
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.example.cgo.ui.theme.Theme
 
@@ -22,24 +26,50 @@ fun SettingsScreen(
     changeTheme: (Theme) -> Unit
 ) {
     Column (modifier = Modifier.selectableGroup()) {
-        Theme.entries.forEach { theme: Theme ->  
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-                    .selectable(
-                        selected = (theme == state.theme),
-                        onClick = { changeTheme(theme) },
-                        role = Role.RadioButton
-                    )
-                    .padding(horizontal = 10.dp),
-                verticalAlignment = Alignment.CenterVertically
+        DropDown(state = state, changeTheme = changeTheme)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDown(
+    state: SettingsState,
+    changeTheme: (Theme) -> Unit
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+    
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp)
+            .padding(horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "App Theme")
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = !isExpanded }
+        ) {
+            TextField(
+                modifier = Modifier.menuAnchor(),
+                value = state.theme.toString(),
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded) }
+            )
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = { isExpanded = false }
             ) {
-                RadioButton(
-                    selected = (theme == state.theme),
-                    onClick = null
-                )
-                Text(text = theme.toString(), modifier = Modifier.padding(start = 16.dp))
+                Theme.entries.forEach { theme: Theme ->
+                    DropdownMenuItem(
+                        text = { Text(text = theme.toString()) },
+                        onClick = {
+                            changeTheme(theme)
+                            isExpanded = false
+                        }
+                    )
+                }
             }
         }
     }
