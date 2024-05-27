70e0ea0d37a02d77beb1672f0db1609238fb59bf
Mattia Forti <mattia.forti2@studio.unibo.it>
2024-05-27 16:35:51 +0200

    fix: fixed flickering in rankings screen

--- app/src/main/java/com/example/cgo/ui/controllers/UsersViewModel.kt
+++ app/src/main/java/com/example/cgo/ui/controllers/UsersViewModel.kt
@@ -8,17 +8,25 @@ import com.example.cgo.data.repositories.UsersRepository
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-data class UsersState(val users: List<User>)
+data class UsersState(
+    val users: List<User>,
+    val usersWithEvents: List<UserWithEvents>
+)
 
 class UsersViewModel(private val repository: UsersRepository) : ViewModel() {
-    val state = repository.users.map { UsersState(users = it) }.stateIn(
+    val state = repository.users.combine(repository.usersWithEvents) { users: List<User>, usersWithEvents: List<UserWithEvents> ->
+        Pair(users, usersWithEvents)
+    }.map { values: Pair<List<User>, List<UserWithEvents>> ->
+        UsersState(users = values.first, usersWithEvents = values.second)
+    }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
-        initialValue = UsersState(emptyList())
+        initialValue = UsersState(emptyList(), emptyList())
     )
 
     fun addUser(user: User) = viewModelScope.launch { repository.upsert(user) }
@@ -26,6 +34,5 @@ class UsersViewModel(private val repository: UsersRepository) : ViewModel() {
     fun deleteUser(user: User) = viewModelScope.launch { repository.delete(user) }
     fun getUserOnLogin(email: String, password: String) : Deferred<User?> = viewModelScope.async { repository.getUserOnLogin(email, password) }
     fun getUserInfo(userId: Int) : Deferred<User?> = viewModelScope.async { repository.getUserInfo(userId) }
-    fun getUsersWithEvents() : Deferred<List<UserWithEvents>> = viewModelScope.async { repository.getUsersWithEvents() }
     fun getUserWithEventsById(userId: Int) : Deferred<UserWithEvents?> = viewModelScope.async { repository.getUserWithEventsById(userId) }
 }
